ce66ccfaddda10206f229ceb4b16989527e1a0ba
Ajay Shakya <aaajjy864@gmail.com>
2023-12-07 19:17:21 +0530

    project1

---
+++ react1/project1/src/App.jsx
@@ -0,0 +1,18 @@
+import Chai from "./chai";
+
+function App() {
+  
+  return (
+    <>
+     <h1>hellow chai aaaaaa</h1>
+     <Chai/>
+    </>
+  )  
+}                                                      
+                                                              
+
+export default App
+
+
+
+
