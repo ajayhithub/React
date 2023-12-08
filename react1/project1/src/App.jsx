4a2a4e72be6332fab3a5d2b80dfbc2eedc6e25b9
Ajay Shakya <aaajjy864@gmail.com>
2023-12-08 19:36:40 +0530

    custom

--- react1/project1/src/App.jsx
+++ react1/project1/src/App.jsx
@@ -1,10 +1,14 @@
 import Chai from "./chai";
 
 function App() {
-  
+     const user = "DIXIT"; 
+
+
   return (
     <>
-     <h1>hellow chai aaaaaa</h1>
+    {/* <h1>hellow chai {user}</h1> in side {} it is not js it is evoluated
+     express here can not js like for loop , if else not write here */}
+     <h1>hellow chai {user}</h1>
      <Chai/>
     </>
   )  
--- react1/project1/src/main.jsx
+++ react1/project1/src/main.jsx
@@ -2,10 +2,40 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+function Myapp(){
+  return(
+    <>
+    <div>
+      <h1>Custom app </h1>
+    </div>
+    </>
+  )
+}
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+   <React.StrictMode>
+    <App/>  {/* you can write as App() */}
+    <Myapp/>    {/* you can write as Myapp() */}
+    ele
+    </React.StrictMode>,
 )
-  
+
+// const ele = (
+//   <a href='https://google.com' target='_blank'>visit google</a>
+// )
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//    ele
+// )
+
+// const user = "ajay";
+// const reactElement = React.createElement(
+//   'a',
+//   {href:'https://google.com', target:'_blank'},
+//   'click visit google'
+//    user
+// )
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   reactElement
+// )
+    
